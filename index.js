a62beb248f67766f01c060ebd9824267dd7a5f19
Harish Nishad <harish.nishad0775@gmail.com>
2021-05-18 00:03:40 +0530

    add despacted Action

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from 'redux';
+import './index.css';
+import App from './components/App';
+import movies from './reducers';
 
-import App from "./components/App";
-import "./index.css";
+
+const store = createStore(movies);
+console.log('store',store);
+console.log('Before STATE',store.getState());
+
+
+store.dispatch({
+    type :'ADD_MOVIES',
+    movies : [{name : 'superman'}]
+})
+console.log('After State',store.getState());
 ReactDOM.render(<App />, document.getElementById("root"));
