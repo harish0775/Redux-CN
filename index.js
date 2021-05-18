e1ff256be22450424eb2dffe4b6ee1ea82ca6b53
Harish Nishad <harish.nishad0775@gmail.com>
2021-05-19 01:59:01 +0530

    Middleware

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore , applyMiddleware} from "redux";
 
 import App from "./components/App";
 import "./index.css";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+//function logger(obj,next,action)
+// logger(obj)(next)(action)
+const logger = function({dispatch,getState}){
+   return function(next){
+       return function(action){
+           //middleware code
+           console.log('ACTION_TYPE = ',action.type);
+           next(action);
+       }
+   }
+}
+const store = createStore(rootReducer, applyMiddleware());
+console.log('store', store);
 
 ReactDOM.render(<App store={store} />, document.getElementById("root"));
