a4b8b70de105bac84a729a6b84943dbc7bede1b8
Harish Nishad <harish.nishad0775@gmail.com>
2021-05-19 17:13:08 +0530

    add file

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,32 @@
 import React from "react";
 
+import { handleMovieSearch } from "../actions";
+
 class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSearchResults: true,
+      searchText: "",
+    };
+  }
+  handleChange = (e) => {
+    this.setState({
+      seachText: e.target.value,
+    });
+  };
+  handleSearch = () => {
+    const { searchText } = this.state;
+    this.props.dispatch(handleMovieSearch(searchText));
+  };
   render() {
     return (
       <div className="navbar">
         <div className="search-container">
-          <input />
-          <button id="search-btn">Search</button>
+          <input onChange={this.handleChange} />
+          <button id="search-btn" onClick={this.handleSearch}>
+            Search
+          </button>
         </div>
       </div>
     );
