ee8b32f86444db3d339aa027c45ab6be26ff20c5
Harish Nishad <harish.nishad0775@gmail.com>
2021-05-24 15:30:19 +0530

    add file

--- practice.js
+++ practice.js
@@ -97,4 +97,32 @@
 //     handleSearchClick = () => {
 //         const { searchText} 
 //     }
-// }
+// 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
+import App from './components/App';
+import rootReducer from './reducers';
+import './index.css';
+
+const logger = ({ dispatch ,getStatee}) => next =>
+action => {
+
+     console.log('ACTION',action);
+     next(action);
+}
+
+const store  = createStore(rootReducer,applyMiddleware(logger,thunk));
+
+console.log('state',store.getState());
+
+
+ReactDOM.render(<App store = {store}/>,document.getElementById('root'));
+
+
+const store = createStore(rootReducer ,applyMiddleware(logger,thunk));
+
+console.log('state',store.getState());
