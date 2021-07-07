51a1ded5bf1f526ce2420526c4e2b5d79a100439
Harish Nishad <harish.nishad0775@gmail.com>
2021-07-07 23:38:00 +0530

    add file

--- index.js
+++ index.js
@@ -44,12 +44,12 @@ console.log('state', store.getState());
 // console.log('state', store.getState()); 
         
 ReactDOM.render(<App store={store} />, document.getElementById('root'));             
-// git add .  
-// git commit -m"add file"  
+// git add .    
+// git commit -m"add file"        
 
        
       
-  
+       
           
 
  
